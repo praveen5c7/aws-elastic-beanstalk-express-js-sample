e309c467ff12a46985273ef2a581d6b479f33e0f
Praveen183 <69027011+praveen5c7@users.noreply.github.com>
2024-02-03 14:00:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Its Aditya Jaiswal--DevOps Shack Youtube Channel'));
+app.get('/', (req, res) => res.send('Its Praveen Naayak, The DevOps Engineer'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
